759c744c419ad644128a3cd069db71537d13bc32
lusuon <JacksonPony@outlook.com>
2018-09-02 22:18:32 +0800

    commit

--- fetch_papers.py
+++ fetch_papers.py
@@ -52,10 +52,10 @@ if __name__ == "__main__":
   # --------------------------parse input arguments 给解析器配置参数——Step 1--------------------------
   parser = argparse.ArgumentParser()
   parser.add_argument('--search-query', type=str,
-                      default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML',
+                      default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML+OR+cat:stat.AP+OR+cat:stat.CO+OR+cat:stat.ME+OR+cat:stat.OT+OR+cat:stat.TH',
                       help='query used for arxiv API. See http://arxiv.org/help/api/user-manual#detailed_examples')
-  parser.add_argument('--start-index', type=int, default=5900, help='0 = most recent API result')
-  parser.add_argument('--max-index', type=int, default=10000, help='upper bound on paper index we will fetch')
+  parser.add_argument('--start-index', type=int, default=0, help='0 = most recent API result')
+  parser.add_argument('--max-index', type=int, default=100000, help='upper bound on paper index we will fetch')
   parser.add_argument('--results-per-iteration', type=int, default=100, help='passed to arxiv API')
   parser.add_argument('--wait-time', type=float, default=5.0, help='lets be gentle to arxiv API (in number of seconds)')
   parser.add_argument('--break-on-no-added', type=int, default=1, help='break out early if all returned query papers are already in db? 1=yes, 0=no')
